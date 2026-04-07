fac830cb17eba177ad26277d55c932b611c45cf2
Blackshadow <blackshadow0916@yandex.ru>
2026-04-07 21:36:25 +0300

    Academy start

---
+++ Academy/Models/Direction.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+    public class Direction
+    {
+        public byte direction_id { get; set; }
+        public string direction_name { get; set; }
+    }
+}
