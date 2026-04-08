9e0025f5af66477a9335ff40a165d5df9a039581
Blackshadow <blackshadow0916@yandex.ru>
2026-04-08 08:13:03 +0300

    Directions DONE

--- Academy/Models/Direction.cs
+++ Academy/Models/Direction.cs
@@ -7,6 +7,6 @@ namespace Academy.Models
     {
         [Key]
         public byte direction_id { get; set; }
-        public string direction_name { get; set; }
+        public string? direction_name { get; set; }
     }
 }
