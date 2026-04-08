f53614b5a3aeb693bf8055d81e637806da4c3f02
Blackshadow <blackshadow0916@yandex.ru>
2026-04-09 00:00:22 +0300

    Direction create_edi_delete DONE

--- Academy/Models/Direction.cs
+++ Academy/Models/Direction.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-
 namespace Academy.Models
 {
     public class Direction
     {
         [Key]
+        [Required(ErrorMessage = "ID is required")]
         public byte direction_id { get; set; }
+
+        [Required(ErrorMessage = "Direction name is required")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 100 characters")]
+        [Display(Name = "Direction Name")]
         public string? direction_name { get; set; }
     }
 }
